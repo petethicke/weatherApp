3c5b7e1c1b50b47cb5acb6ea55ec99b18bdf4c7d
Peter Thicke <peterbthicke@gmail.com>
2015-06-02 17:58:37 -0700

    autocomplete working

--- script.js
+++ script.js
@@ -3,7 +3,41 @@ $(function() {
   $('#search').on('keyup', function(e) {
     e.preventDefault();
     var query = $('#search').val();
-    console.log(query);
+    $.ajax ({
+      url: 'http://autocomplete.wunderground.com/aq?query=' + query + "&cb=callback",
+      type: 'GET',
+      dataType: 'jsonp',
+      jsonpCallback:'callback',
+      // error: function() {
+      // },
+      success: function(data) {
+        console.log(query);
+        $('#results').empty();
+        $('#weather').empty();
+        $.each(data, function(i, cities){
+          cities.forEach(function(city){
+            var $list = $("<li>").text(city.name);
+            $list.appendTo('#results');
+          });
+        });
+      }
+    });
+  });
+
+  $('#searchForm').on('submit', function(e) {
+    e.preventDefault();
+    var query = $('#search').val();
+    $.ajax ({
+      url: 'http://api.wunderground.com/api/2516967/conditions/q/CA/San_Francisco.json',
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data) {
+        $('data').appendTo('#weather');
+      }
+    });
+
   });
 
 });
+
+
