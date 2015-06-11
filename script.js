8c5aab9f2f1ef4adb6d99c1cf569d0329f887cdb
Peter Thicke <peterbthicke@gmail.com>
2015-06-10 20:50:58 -0700

    api calls functional

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function() {
-  
+
   $('#search').on('keyup', function(e) {
     e.preventDefault();
     var query = $('#search').val();
@@ -14,30 +14,42 @@ $(function() {
         console.log(query);
         $('#results').empty();
         $('#weather').empty();
-        $.each(data, function(i, cities){
-          cities.forEach(function(city){
-            var $list = $("<li>").text(city.name);
+        $.each(data, function(i, places){
+          places.forEach(function(place){
+            var $list = $("<li>").text(place.name);
             $list.appendTo('#results');
+            $("li").on('click', function(){
+              var city = $(this).text();
+              $("#input").val(city);
+            });
           });
         });
       }
     });
   });
 
-  $('#searchForm').on('submit', function(e) {
+  $('#btn').on('click', function(e) {
     e.preventDefault();
     var query = $('#search').val();
+    //console.log(query);
     $.ajax ({
-      url: 'http://api.wunderground.com/api/2516967/conditions/q/CA/San_Francisco.json',
+      url: 'http://api.wunderground.com/api/08cfbef71abd6291/conditions/q/' + query + ".json?callback=jsonCallback",
       type: 'GET',
       dataType: 'jsonp',
+      jsonpCallback:'callback',
       success: function(data) {
-        $('data').appendTo('#weather');
+        $('#results').empty();
+        $('#weather').empty();
+        if(data.current_observation){
+          var cityName = data.current_observation.display_location.city;
+          var conditions = data.current_observation.weather;
+          $('<h3>').appendTo('#results').html(cityName);
+          $('<p>').appendTo('#results').html(conditions);
+        }
       }
     });
-
   });
-
 });
+
 
 
